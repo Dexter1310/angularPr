eb74ac4212b644fe21104ec3e9523971384a6561
javi <jo@ascetic.io>
2021-03-01 09:18:20 +0100

    first commit

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -11,17 +11,25 @@ import {Item} from '../../models/item'
 export class ItemsComponent implements OnInit {
 
   items:Item[]=[];
+  total:number=0;
   @Output()deleteItem:EventEmitter<Item>=new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
-    this.items=[{ide:0,title:'dron',quantity:2,price:200,completed:true},{ide:2,title:'dron',quantity:2,price:200,completed:false}];
+    this.items=[{ide:0,title:'dron',quantity:2,price:200,completed:true},{ide:2,title:'dron',quantity:2,price:300,completed:false},{ide:3,title:'dron',quantity:3,price:100,completed:false}];
   }
 
   onDelete(idde:Item){//delete element
     this.items = this.items.filter(item => item.ide !== idde.ide);
+    alert(this.getTotal())
+
   }
   onToggle(idde:Item){
-    alert(idde.quantity*idde.price)
+    idde.completed=!idde.completed;
+  }
+  getTotal(){
+    return this.items.filter(item=>!item.completed)
+     .map(item=>item.quantity*item.price);
+
   }
 }
